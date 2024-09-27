6546b7a2cd9bef96525b5da7c0d459fa4d0e3542
Gunski <gunnarkryth@gmail.com>
2024-09-27 09:36:40 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo off
 
-start "" cmd /c "npm install && npm install sass && npm install react-router-dom && npm install tanstack && graphql-request && markdown-to-react && npm run dev"
+start "" cmd /c "npm install && npm install sass && npm install react-router-dom && npm install @tanstack/react-query && npm install graphql-request && npm audit fix && npm run dev"
 timeout /t 10
 
 start http://localhost:5173/
